29df02c7504fd960d98e2d347f8bb2d54ea0028e
adityrajtiwary <122718170+adityrajtiwary@users.noreply.github.com>
2024-07-03 11:58:50 +0530

    Add files via upload

---
+++ Lecture4-CipherSchool.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b = 0;
+	
+	cout<<"enter the value of a and b: ";
+	cin>>a>>b;
+	
+	cout<<"Sum of a and b is: "<<a+b<<endl;
+	
+	
+	return 0;
+}
